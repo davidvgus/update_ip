a67fb9039fa14bc90c042a91f0a01676dbc13e17
David Gustafson <git@radicalskeptic.net>
2013-02-21 22:53:37 -0800

    Added shell script for easy cron job use of ruby script.

---
+++ update-ip-address.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#use a line like the one below if you are using rvm. Modify it to represent
+#your ruby version and gemset.
+
+#source /home/example_user/.rvm/environments/ruby-1.9.3-p286@example_gemset
+
+#Put the changes on a webpage that you have access to.
+
+#ruby example_path_to_script/update_ip/update_ip.rb |ssh -i /home/dave/.ssh/id_rsa example_user@host "cat > public_html/address.html"
+
+
